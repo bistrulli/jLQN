afb2a27c7ce262b523e270450dc90cf0715f9f16
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-02-05 12:53:04 +0100

    Add argument input

--- random-LQN-generator/lqn_generator.py
+++ random-LQN-generator/lqn_generator.py
@@ -1,7 +1,23 @@
 import random
 import os
+import argparse
 
 out_lqn_folder = os.path.join(os.getcwd(), "LQNs")
+
+
+def get_cli():
+    """
+    Get input arguments from CLI.
+    :return:    ArgumentParser object.
+    """
+    parser = argparse.ArgumentParser(description="Generate a set of random LQNs.")
+    parser.add_argument("-n", "--number", type=int, default=10,
+                        help='The number of LQNs to generate (default is 10).', required=False)
+    parser.add_argument("-f", "--functions", type=int,
+                        help='The number of functions for each LQN.', required=True)
+    return parser.parse_args()
+
+
 
 def random_service_time():
     return round(random.uniform(0.1, 4.0), 2)
@@ -107,65 +123,35 @@ def activities_declaration(tasks, dag):
         text += "-1\n\n"
     return text
 
-def activities_declaration_old(tasks, num_activities):
-    text = ""
-    text += "# Activities declaration\n"
-    for i, task in enumerate(tasks):
-        text += f"A {task}\n"
-        # Service times
-        for j in range(num_activities[i]):
-            text += f"s acti{i}_{j} {random_service_time()}\n"
-        # Calls (activity graph)
-        if i != len(tasks)-1:
-            for j in range(num_activities[i]):
-                #if random.random() < 0.5: # Do it 50% of the times
-                if i != len(tasks) and j == 0: # Do it 50% of the times
-                    #tgt_entry_num = random.choice([x for x in range(i+1, len(tasks)+1)])
-                    tgt_entry_num = i+1
-                    tgt_entry = f"Entr{tgt_entry_num}"
-                    text += f"y acti{i}_{j} {tgt_entry} 1.0\n:\n"
-                    text += f"  acti{i}_{j} -> acti{i}_{j+1};\n"
-                    if i != 0:
-                        text += f"  acti{i}_{j+1}[Entr{i}]\n"
-            text += "-1\n\n"
-        else:
-            text += f":\n  acti{i}_0[Entr{i}]\n-1\n\n"
-    return text
-
-
 def generate_random_lqn(filename, num_tasks, max_entries_per_task=1, max_activities_per_entry=3):
-    processors = [f"Proc{i}" for i in range(num_tasks)]
-    tasks = [f"Task{i}" for i in range(num_tasks)]
-    entries = [f"Entr{i}" for i in range(num_tasks)]
-    #num_activities = [random.randint(1, max_activities_per_entry) for i in range(num_tasks)]
-    #num_activities = [2 for i in range(num_tasks)]
-    #num_activities[num_tasks-1] = 1
-
     lqn_text = header_declaration(filename)
+
+    processors = [f"Proc{i}" for i in range(num_tasks)]
     lqn_text += processors_declaration(processors)
+
+    tasks = [f"Task{i}" for i in range(num_tasks)]
     lqn_text += tasks_declaration(tasks)
+
+    # One entry per task
+    entries = [f"Entr{i}" for i in range(num_tasks)]
     lqn_text += entries_declaration(entries)
-    #lqn_text += activities_declaration(tasks, num_activities)
     
     dag = generate_random_dag_with_one_root(num_tasks, 0.5, 0.3)
     lqn_text += activities_declaration(tasks, dag)
 
-
-    print(lqn_text)
     save_file(filename, lqn_text)
 
-# Generate and print a random LQN model
-# max_tasks = 10
-# num_tasks = random.randint(3, max_tasks)
-max_entries_per_task = 1
-max_activities_per_entry = 3
-# avg_call_number
+    return lqn_text
 
+
+
+if __name__ == '__main__':
+    args = get_cli()
+    for i in range(0, args.number-1):
+        lqn_text = generate_random_lqn(f'lqn{i}.lqn', args.functions)
+        # print(lqn_text)
+
+
+# TODO
 # num chiamate medie per connessione (distribuz gaussiana)
 # flag (chiamate asincrone?) con relativa probabilità
-
-
-
-num_functions = 10
-for i in range(0,9):
-    generate_random_lqn(f'lqn{i}.lqn', num_functions)
