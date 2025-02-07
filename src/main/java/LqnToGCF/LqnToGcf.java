329b27c19e215391f05d5fd84052d02de181b30b
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-02-07 11:10:13 +0100

    Change reference to project path

--- src/main/java/LqnToGCF/LqnToGcf.java
+++ src/main/java/LqnToGCF/LqnToGcf.java
@@ -21,170 +21,168 @@ import java.util.Properties;
 
 import Entity.App;
 import Entity.Function;
+import main.Main;
 
 public class LqnToGcf {
-	// Find the current path (run the jar file from the root of the project)
-	private static String projectRoot = System.getProperty("user.dir");
-	private static Path projectPath = Paths.get(projectRoot);
+    
+    private Path destPath = Main.projectPath.resolve("output");
+    private VelocityEngine velocityEngine;
+    private static Path tmpPath = Main.projectPath.resolve("resources/f_tmpl");
+    private static Path tmpLocustPath = Main.projectPath.resolve("resources/driver_tmpl");
+    private static Path tmpSysScriptsPath = Main.projectPath.resolve("resources/scripts_sys_tmpl");
 
-	private Path destPath = projectPath.resolve("output");
-	private VelocityEngine velocityEngine;
-	private static Path tmpPath = projectPath.resolve("resources/f_tmpl");
-	private static Path tmpLocustPath = projectPath.resolve("resources/driver_tmpl");
-	private static Path tmpSysScriptsPath = projectPath.resolve("resources/scripts_sys_tmpl");
-
-	public LqnToGcf(App lqnApp) {
-		
-		//init velocity
-		Properties props = new Properties();
+    public LqnToGcf(App lqnApp) {
+        
+        //init velocity
+        Properties props = new Properties();
         props.setProperty("resource.loader", "file");
         props.setProperty("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.FileResourceLoader");
 
         // Abilita i percorsi assoluti
         props.setProperty("file.resource.loader.path", ""); // Percorso vuoto per consentire path assolute
         props.setProperty("file.resource.loader.cache", "false"); // Opzionale: evita cache durante sviluppo
-		
+        
         this.velocityEngine = new VelocityEngine();
         this.velocityEngine.init(props);
-		
-		Path appDir = Paths.get(destPath.toString() + File.separator + lqnApp.getName().replace("\"", ""));
-		for (Function f : lqnApp.getFunctions()) {
-			if (!f.getKind().equals("r")) {
-				this.copyTmpfun(tmpPath, appDir, f);
-				this.translate(appDir, f);
-			} else {
-				this.copyTmpfun(tmpLocustPath, appDir, f);
-				this.translateLocust(appDir, f);
-			}
-		}
-		//copy file at the lqnsystem level
-		this.copySysScritps(tmpSysScriptsPath, appDir);
-	}
-	
-	public void copySysScritps(Path source, Path dest) {
-		try {
-			FileUtils.createParentDirectories(dest.toFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		File sourceDirectory = new File(source.toString());
-		File destinationDirectory = new File(dest.toString());
-		try {
-			FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        
+        Path appDir = Paths.get(destPath.toString() + File.separator + lqnApp.getName().replace("\"", ""));
+        for (Function f : lqnApp.getFunctions()) {
+            if (!f.getKind().equals("r")) {
+                this.copyTmpfun(tmpPath, appDir, f);
+                this.translate(appDir, f);
+            } else {
+                this.copyTmpfun(tmpLocustPath, appDir, f);
+                this.translateLocust(appDir, f);
+            }
+        }
+        //copy file at the lqnsystem level
+        this.copySysScritps(tmpSysScriptsPath, appDir);
+    }
+    
+    public void copySysScritps(Path source, Path dest) {
+        try {
+            FileUtils.createParentDirectories(dest.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File sourceDirectory = new File(source.toString());
+        File destinationDirectory = new File(dest.toString());
+        try {
+            FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void translateLocust(Path fDir, Function f) {
-		fDir = Paths.get(fDir.toString() + File.separator + f.getName());
-		
-		VelocityContext context = new VelocityContext();
-		context.put("f", f);
-		Template template = null;
+    public void translateLocust(Path fDir, Function f) {
+        fDir = Paths.get(fDir.toString() + File.separator + f.getName());
+        
+        VelocityContext context = new VelocityContext();
+        context.put("f", f);
+        Template template = null;
 
-		try {
-			template = this.velocityEngine.getTemplate(tmpLocustPath.toString() + File.separator + "SimpleWorkload.vm");
-		} catch (ResourceNotFoundException rnfe) {
-			rnfe.printStackTrace();
-		} catch (ParseErrorException pee) {
-			pee.printStackTrace();
-		} catch (MethodInvocationException mie) {
-			mie.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            template = this.velocityEngine.getTemplate(tmpLocustPath.toString() + File.separator + "SimpleWorkload.vm");
+        } catch (ResourceNotFoundException rnfe) {
+            rnfe.printStackTrace();
+        } catch (ParseErrorException pee) {
+            pee.printStackTrace();
+        } catch (MethodInvocationException mie) {
+            mie.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		StringWriter sw = new StringWriter();
-		template.merge(context, sw);
-		//System.out.println(sw.toString());
-		try {
-			FileWriter fw = new FileWriter(
-					Paths.get(fDir.toString() + File.separator + "SimpleWorkload.py").toFile());
-			fw.write(sw.toString());
-			fw.flush();
-			fw.close();
-			FileUtils.delete(Paths.get(fDir.toString() + File.separator + "SimpleWorkload.vm").toFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        StringWriter sw = new StringWriter();
+        template.merge(context, sw);
+        //System.out.println(sw.toString());
+        try {
+            FileWriter fw = new FileWriter(
+                    Paths.get(fDir.toString() + File.separator + "SimpleWorkload.py").toFile());
+            fw.write(sw.toString());
+            fw.flush();
+            fw.close();
+            FileUtils.delete(Paths.get(fDir.toString() + File.separator + "SimpleWorkload.vm").toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void translate(Path fDir, Function f) {
-		fDir = Paths.get(fDir.toString() + File.separator + f.getName());
-		
-		VelocityContext context = new VelocityContext();
-		context.put("f", f);
-		Template template = null;
+    public void translate(Path fDir, Function f) {
+        fDir = Paths.get(fDir.toString() + File.separator + f.getName());
+        
+        VelocityContext context = new VelocityContext();
+        context.put("f", f);
+        Template template = null;
 
-		try {
-			template = this.velocityEngine.getTemplate(tmpPath.toString() + "/src/main/java/functions/Logic.vm");
-		} catch (ResourceNotFoundException rnfe) {
-			rnfe.printStackTrace();
-		} catch (ParseErrorException pee) {
-			pee.printStackTrace();
-		} catch (MethodInvocationException mie) {
-			mie.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            template = this.velocityEngine.getTemplate(tmpPath.toString() + "/src/main/java/functions/Logic.vm");
+        } catch (ResourceNotFoundException rnfe) {
+            rnfe.printStackTrace();
+        } catch (ParseErrorException pee) {
+            pee.printStackTrace();
+        } catch (MethodInvocationException mie) {
+            mie.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		StringWriter sw = new StringWriter();
-		template.merge(context, sw);
-		try {
-			FileWriter fw = new FileWriter(
-					Paths.get(fDir.toString() + File.separator + "/src/main/java/functions/Logic.java").toFile());
-			fw.write(sw.toString());
-			fw.flush();
-			fw.close();
-			FileUtils
-					.delete(Paths.get(fDir.toString() + File.separator + "/src/main/java/functions/Logic.vm").toFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        StringWriter sw = new StringWriter();
+        template.merge(context, sw);
+        try {
+            FileWriter fw = new FileWriter(
+                    Paths.get(fDir.toString() + File.separator + "/src/main/java/functions/Logic.java").toFile());
+            fw.write(sw.toString());
+            fw.flush();
+            fw.close();
+            FileUtils
+                    .delete(Paths.get(fDir.toString() + File.separator + "/src/main/java/functions/Logic.vm").toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	private void copyTmpfun(Path source, Path dest, Function f) {
-		dest = Paths.get(dest.toString() + File.separator + f.getName());
-		try {
-			FileUtils.createParentDirectories(dest.toFile());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		File sourceDirectory = new File(source.toString());
-		File destinationDirectory = new File(dest.toString());
-		try {
-			FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if (Paths.get(dest.toString() + File.separator + "pom.xml").toFile().exists())
-			this.updateFname(Paths.get(dest.toString() + File.separator + "pom.xml"), f.getName());
-		if (Paths.get(dest.toString() + File.separator + "deploy.sh").toFile().exists())
-			this.updateFname(Paths.get(dest.toString() + File.separator + "deploy.sh"), f.getName());
-		if (Paths.get(dest.toString() + File.separator + "update.sh").toFile().exists())
-			this.updateFname(Paths.get(dest.toString() + File.separator + "update.sh"), f.getName());
-	}
+    private void copyTmpfun(Path source, Path dest, Function f) {
+        dest = Paths.get(dest.toString() + File.separator + f.getName());
+        try {
+            FileUtils.createParentDirectories(dest.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File sourceDirectory = new File(source.toString());
+        File destinationDirectory = new File(dest.toString());
+        try {
+            FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Paths.get(dest.toString() + File.separator + "pom.xml").toFile().exists())
+            this.updateFname(Paths.get(dest.toString() + File.separator + "pom.xml"), f.getName());
+        if (Paths.get(dest.toString() + File.separator + "deploy.sh").toFile().exists())
+            this.updateFname(Paths.get(dest.toString() + File.separator + "deploy.sh"), f.getName());
+        if (Paths.get(dest.toString() + File.separator + "update.sh").toFile().exists())
+            this.updateFname(Paths.get(dest.toString() + File.separator + "update.sh"), f.getName());
+    }
 
-	public void updateFname(Path file, String fname) {
-		File f = file.toFile();
-		try {
-			Scanner sc = new Scanner(f);
-			String content = "";
-			while (sc.hasNextLine())
-				content += sc.nextLine() + "\n";
-			sc.close();
-			content = content.replace("$fname", fname.toLowerCase());
-			try {
-				FileWriter fw = new FileWriter(f);
-				fw.write(content);
-				fw.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    public void updateFname(Path file, String fname) {
+        File f = file.toFile();
+        try {
+            Scanner sc = new Scanner(f);
+            String content = "";
+            while (sc.hasNextLine())
+                content += sc.nextLine() + "\n";
+            sc.close();
+            content = content.replace("$fname", fname.toLowerCase());
+            try {
+                FileWriter fw = new FileWriter(f);
+                fw.write(content);
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 
 import org.antlr.v4.runtime.CharStreams;
@@ -22,57 +24,62 @@ import LQN2MPP.LqnToMPP;
 import LqnToGCF.LqnToGcf;
 
 /**
- * @author Tom Everett
+ * Author: Tom Everett
  */
-class Main {
-	public static void main(String[] args) {
-		// Find the current path (run the jar file from the root of the project)
-		String projectRoot = System.getProperty("user.dir");
-		String directoryPath = projectRoot + "/resources/wasteless_journal";
-		String wildcardPattern = "*.lqn";
-		try {
-			Collection<File> matchingFiles = FileUtils.listFiles(new File(directoryPath),
-					new WildcardFileFilter(wildcardPattern), null);
-			if (matchingFiles.isEmpty()) {
-				System.out.println("No files found matching the pattern '" + wildcardPattern + "' in directory '"
-						+ directoryPath + "'");
-			} else {
-				System.out.println("Matching files:");
-				for (File f : matchingFiles) {
-					System.out.println(f.getName());
-					/*
-					 * get the input file as an InputStream
-					 */
-					// File f = new File("src/main/resources/case_studies/sockshop.lqn");
-					InputStream inputStream = new FileInputStream(f);
-					/*
-					 * make Lexer
-					 */
-					Lexer lexer = new JLqnLexer(CharStreams.fromStream(inputStream));
-					/*
-					 * get a TokenStream on the Lexer
-					 */
-					TokenStream tokenStream = new CommonTokenStream(lexer);
-					/*
-					 * make a Parser on the token stream
-					 */
-					JLqnParser parser = new JLqnParser(tokenStream);
-					MyJqnListner myListner = new MyJqnListner();
-					parser.addParseListener(myListner);
-					/*
-					 * get the top node of the AST. This corresponds to the topmost rule of
-					 * equation.q4, "equation"
-					 */
-					@SuppressWarnings("unused")
-					RuleModelContext ruleModel = parser.ruleModel();
+public class Main {
+	
+    static public Path projectPath;
+        
+    public static void main(String[] args) {
+		Path currentFilePath = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).toAbsolutePath();
+        projectPath = currentFilePath.getParent().getParent(); 
+        // Find the current path (run the jar file from the root of the project)
+        
+        Path directoryPath = projectPath.resolve("resources/wasteless_journal");
+        String wildcardPattern = "*.lqn";
+        try {
+            Collection<File> matchingFiles = FileUtils.listFiles(directoryPath.toFile(),
+                    new WildcardFileFilter(wildcardPattern), null);
+            if (matchingFiles.isEmpty()) {
+                System.out.println("No files found matching the pattern '" + wildcardPattern + "' in directory '"
+                        + directoryPath + "'");
+            } else {
+                System.out.println("Matching files:");
+                for (File f : matchingFiles) {
+                    System.out.println(f.getName());
+                    /*
+                     * get the input file as an InputStream
+                     */
+                    // File f = new File("src/main/resources/case_studies/sockshop.lqn");
+                    InputStream inputStream = new FileInputStream(f);
+                    /*
+                     * make Lexer
+                     */
+                    Lexer lexer = new JLqnLexer(CharStreams.fromStream(inputStream));
+                    /*
+                     * get a TokenStream on the Lexer
+                     */
+                    TokenStream tokenStream = new CommonTokenStream(lexer);
+                    /*
+                     * make a Parser on the token stream
+                     */
+                    JLqnParser parser = new JLqnParser(tokenStream);
+                    MyJqnListner myListner = new MyJqnListner();
+                    parser.addParseListener(myListner);
+                    /*
+                     * get the top node of the AST. This corresponds to the topmost rule of
+                     * equation.q4, "equation"
+                     */
+                    @SuppressWarnings("unused")
+                    RuleModelContext ruleModel = parser.ruleModel();
 
-					LqnToGcf gcftrasducer = new LqnToGcf(myListner.app);
-					//LqnToMPP mpptrasducer = new LqnToMPP(myListner.app);
-				}
-			}
+                    LqnToGcf gcftrasducer = new LqnToGcf(myListner.app);
+                    //LqnToMPP mpptrasducer = new LqnToMPP(myListner.app);
+                }
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
