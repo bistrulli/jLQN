4f3db1dd2c154127b2e47570b13a035802e3df00
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-03-04 15:45:08 +0100

    General improvements

--- log_to_csv.py
+++ log_to_csv.py
@@ -10,13 +10,15 @@ def turn_log_into_csv(lqn_name, entr_name):
 
     # Regex patterns to match the log entries
     time_pattern = re.compile(r'^(.*) functions\.Logic service')
-    info_pattern = re.compile(r'INFO: cpu:=(\d+)  rt:=(\d+)$')
+    info_pattern = re.compile(r'INFO: cpu:=(-?\d+)  rt:=(-?\d+)$')
 
     with open(log_file_path, 'r') as log_file, open(csv_file_path, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         # Header
         csv_writer.writerow(['date-time', 'cpu', 'rt'])
         
+        date_time_formatted = None
+
         for line in log_file:
             match_time = time_pattern.match(line.strip())
             match_info = info_pattern.match(line.strip())
@@ -26,14 +28,17 @@ def turn_log_into_csv(lqn_name, entr_name):
                 # Convert the date-time string to a standard format
                 date_time = datetime.strptime(date_time_str, '%b %d, %Y %I:%M:%S %p')
                 date_time_formatted = date_time.strftime('%Y-%m-%d %H:%M:%S')
+                #print(f"Matched time: {date_time_formatted}")
 
-            if match_info: # e.g. INFO: cpu:=73258820  rt:=3136098152
+            if match_info and date_time_formatted: # e.g. INFO: cpu:=73258820  rt:=3136098152
                 cpu = match_info.group(1)
                 rt = match_info.group(2)
                 # Write the extracted values to the CSV file
                 csv_writer.writerow([date_time_formatted, cpu, rt])
+                #print(f"Matched info: cpu={cpu}, rt={rt}")
+                date_time_formatted = None
 
-    print(f'Log file parsed and saved to {csv_file_path}')
+    #print(f'Log file parsed and saved to {csv_file_path}')
 
 def calculate_average(csv_file_path, column_name):
     values = []
@@ -47,9 +52,9 @@ def calculate_average(csv_file_path, column_name):
     else:
         return None
 
-lqn_name = '5fun-lqn0-template.lqn'
+lqn_name = 'lqn00-9functions.lqn'
 
-for i in range(1, 5):
+for i in range(1, 9):
     entr_name = f'Entr{i}'
     turn_log_into_csv(lqn_name, entr_name)
 
@@ -58,5 +63,4 @@ for i in range(1, 5):
     # Calculate the average rt value
     average_rt = calculate_average(csv_file_path, 'rt')
     average_cpu = calculate_average(csv_file_path, 'cpu')
-    print(f'The average rt value is: {average_rt}')
-    print(f'The average cpu value is: {average_cpu}')
+    print(f'{entr_name}: {round(average_rt/1000000)}')
