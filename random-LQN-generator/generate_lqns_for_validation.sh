ba1edee92220f6eb3fa14a9fb3e658e16c0a4729
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-04-18 09:42:09 +0200

    Commented lqns line

--- random-LQN-generator/generate_lqns_for_validation.sh
+++ random-LQN-generator/generate_lqns_for_validation.sh
@@ -3,6 +3,6 @@
 python3 lqn_generator_jinja2.py --num-lqns 3 --max-functions 6 --min-functions 6 \
                                 --prob-edge 0.6 --prob-async 0 --prob-parallel 0
 
-python3 process_lqns_into_lqx.py
+#python3 process_lqns_into_lqx.py
 
 #cp LQNs/* ../resources/wasteless_journal/
