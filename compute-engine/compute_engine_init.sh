1b31509f03f3665575c3c43d720a5ef681f1e0bc
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-04-30 16:08:33 +0200

    Remove file

--- compute-engine/compute_engine_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Step 1: Install Python dependencies
-echo "Installing Python dependencies..."
-sudo pip install -r requirements.txt
-
-# Step 2: Execute the Prometheus script
-echo "Deploying Prometheus and Prometheus Pushgateway..."
-./prometheus/run_prometheus.sh
